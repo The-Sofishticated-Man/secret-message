548a6a5f0280a03c55350d1971388c96c7368759
islam <lameche1islam@gmail.com>
2024-06-21 17:29:28 +0100

    batman

---
+++ client/secret-message/src/components/NavBar.tsx
@@ -0,0 +1,11 @@
+import "./NavBar"
+const NavBar = () => {
+  return (
+    <nav>
+      <button className='nav__button--outline'>Log in</button>
+      <button className='nav__button--fill'>Sign up</button>
+    </nav>
+  )
+}
+
+export default NavBar

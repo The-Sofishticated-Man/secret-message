7b7bdea3cfd3d530113e19b172feb4d93e1b0127
islam <lameche1islam@gmail.com>
2024-06-27 15:34:29 +0100

    Added footer

--- client/secret-message/src/App.tsx
+++ client/secret-message/src/App.tsx
@@ -1,12 +1,14 @@
 import NavBar from "./components/NavBar/Index";
 import Hero from "./components/Hero";
 import HowToUse from "./components/HowToUse";
+import Footer from "./components/Footer/Index";
 function App() {
   return (
     <>
       <NavBar />
       <Hero />
       <HowToUse />
+      <Footer/>
     </>
   );
 }

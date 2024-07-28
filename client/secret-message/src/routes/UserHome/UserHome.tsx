c546eb6b37bc8d9bbe073632ecf694b9c73a26ea
islam <lameche1islam@gmail.com>
2024-07-28 16:29:57 +0100

    improved copy button's design

--- client/secret-message/src/routes/UserHome/UserHome.tsx
+++ client/secret-message/src/routes/UserHome/UserHome.tsx
@@ -6,28 +6,32 @@ import {
 } from "react-icons/fa6";
 import style from "./UserHome.module.css";
 import Cookies from "js-cookie";
-import { IoCopy } from "react-icons/io5";
-
+import { FaCopy } from "react-icons/fa";
 const UserHome = () => {
   const userCookies = JSON.parse(Cookies.get("SMUser")!);
   const link = `http://localhost:${import.meta.env.VITE_PORT}/send/${
     userCookies.id
   }`;
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(link);
+  };
   return (
     <section className={style.userHomeSection}>
       <h1 className={style.userHeading}>Welcome {userCookies.user} !</h1>
       <div className={style.userLink}>
         <h3>Your link has been generated :</h3>
-        <input readOnly value={link} />
+        <div className={style.userLinkContainer}>
+         <input className={style.userLinkInput} type="text" readOnly value={link}  /> 
+         <FaCopy className={style.copyIcon} onClick={copyToClipboard} size={35}  />
+        </div>
         <p className={style.userLinkText}>
           share this link with your friends to receive secret messages !
         </p>
         <div className={style.userLinkShareOptions}>
-          <FaSquareFacebook  color="#1877F2" />
+          <FaSquareFacebook color="#1877F2" />
           <FaSquareXTwitter color="#1DA1F2" />
           <FaSquareInstagram color="#E1306C" />
           <FaSquareWhatsapp color="#25D366" />
-          <IoCopy color="#0B0B0B" />
         </div>
       </div>
     </section>

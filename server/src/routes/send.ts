33dfece783927a77534c68262f4a78b21263af85
islam <lameche1islam@gmail.com>
2024-07-29 16:16:05 +0100

    added client side sanitization

--- server/src/routes/send.ts
+++ server/src/routes/send.ts
@@ -1,44 +1,35 @@
 import express from "express";
 import SMUser from "../utils/mongoAPIUtils";
-import { body, validationResult } from "express-validator";
+import xss from "xss";
 const router = express.Router();
 
-router.post(
-  "/:userId",
-  body("secretMessage").exists().trim().escape().notEmpty(),
-  (req, res) => {
-    const result = validationResult(req).array();
-    const userId = req.params?.userId;
-    console.log("got request to userId: ", userId);
-    const secretMessage = req.body.secretMessage;
-    console.log("with a payload of: ", secretMessage);
-    console.log("validation result:", result);
-    if (result.length !== 0) {
-      return res.status(400).json({ errors: result.map((error) => error.msg) });
-    }
-    if (!secretMessage) {
-      res
-        .status(400)
-        .json({ error: "you didn't send a secretMessage dumbfuck" });
-    } else {
-      SMUser.updateOne(
-        { _id: userId },
-        {
-          $push: {
-            secretMessages: { message: secretMessage, date: new Date() },
-          },
-        }
-      )
-        .then((value) => {
-          console.log("updated user: ", value);
-          res.json({ message: "secret meassage sent successfully" });
-        })
-        .catch((error) => {
-          console.error(error);
-          res.status(500).json({ error });
-        });
-    }
+router.post("/:userId", (req, res) => {
+  const userId = req.params?.userId;
+  console.log("got request to userId: ", userId);
+  const secretMessage = req.body.secretMessage;
+  console.log("with a message of: ", secretMessage);
+  const sanitizedMessage = xss(secretMessage);
+  console.log("sanitized message: ", sanitizedMessage);
+  if (!secretMessage) {
+    res.status(400).json({ error: "you didn't send a secretMessage dumbfuck" });
+  } else {
+    SMUser.updateOne(
+      { _id: userId },
+      {
+        $push: {
+          secretMessages: { message: sanitizedMessage, date: new Date() },
+        },
+      }
+    )
+      .then((value) => {
+        console.log("updated user: ", value);
+        res.json({ message: "secret meassage sent successfully" });
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).json({ error });
+      });
   }
-);
+});
 
 export default router;

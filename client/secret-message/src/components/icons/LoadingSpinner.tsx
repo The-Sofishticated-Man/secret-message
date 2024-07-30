8c4eaf161bbd4db7c61e9cf376b43c1e892ca0f6
islam <lameche1islam@gmail.com>
2024-07-30 15:59:01 +0100

    improved button design

--- client/secret-message/src/components/icons/LoadingSpinner.tsx
+++ client/secret-message/src/components/icons/LoadingSpinner.tsx
@@ -1,7 +1,7 @@
 import "ldrs/ring";
-const loadingSpinner = () => {
+const loadingSpinner = ({ Color }: { Color?: string }) => {
   //just a disclaimer this always returns an error even tho it works perfectly fine, I honestly just don't know how to fix it trust me I've tried my hardest
-  return <l-ring color="var(--color-secondary)" size={31}></l-ring>;
+  return <l-ring color={Color || "white"} size={30}></l-ring>;
 };
 
 export default loadingSpinner;

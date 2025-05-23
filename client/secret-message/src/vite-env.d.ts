c6907237b4cd0293c4d55dc0c3d74aa5a4019264
Islam lameche <lameche1islam@gmail.com>
2025-05-23 12:37:01 +0100

    fixed loading ring and installed github pages

--- client/secret-message/src/vite-env.d.ts
+++ client/secret-message/src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+declare namespace JSX {
+  interface IntrinsicElements {
+    "l-ring": React.DetailedHTMLProps<
+      React.HTMLAttributes<HTMLElement>,
+      HTMLElement
+    > & {
+      color?: string;
+      size?: number;
+    };
+  }
+}

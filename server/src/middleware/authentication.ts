e3532e354610b502ecf0852703443be62bef3948
Islam lameche <lameche1islam@gmail.com>
2025-05-25 15:34:23 +0100

    extended request type to allow for user piggyback

--- server/src/middleware/authentication.ts
+++ server/src/middleware/authentication.ts
@@ -14,7 +14,7 @@ export default async function authenticate(
   next: NextFunction
 ) {
   const { authorization } = req.headers;
-  logger.info("got authorization request with payload: ",authorization);
+  logger.info("got authorization request with payload: ", authorization);
   if (!authorization) {
     // authorization header empty
     logger.error("authorization header is empty");
@@ -29,7 +29,6 @@ export default async function authenticate(
       process.env.JWT_SECRET_KEY as string
     ) as JwtPayload;
 
-    // piggy back userId to the request object
     req.user = (await SMUser.findById({ _id }).select("_id")) as object;
     logger.info("request authenticated successfully as user: ", req.user);
   } catch (error) {

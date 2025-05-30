a6ed9f6e185ff3613f9f333f6db32086d606e60b
islam <lameche1islam@gmail.com>
2025-05-30 12:53:41 +0100

    Simplify logout route by removing user check before clearing refresh token cookie

--- server/src/routes/logout.ts
+++ server/src/routes/logout.ts
@@ -3,10 +3,8 @@ import express, { Request, Response } from "express";
 const router = express.Router();
 
 router.get("/", (req: Request, res: Response) => {
-  if (req.user) {
-    res.clearCookie("refreshToken");
-    res.status(200).json({ message: "Logged out successfully" });
-  }
+  res.clearCookie("refreshToken");
+  res.status(200).json({ message: "Logged out successfully" });
 });
 
 export default router;
